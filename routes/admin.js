23b3c7c140bd19b0132eefe9e3f02cf6b065956a
Asiful Islam Sakib <asif@magnetismtech.com>
2022-10-06 00:33:16 +0600

    sharing data between js files

--- routes/admin.js
+++ routes/admin.js
@@ -8,30 +8,17 @@ const router = express.Router()
 const ADD_PRODUCT_ROUTE = 'add-product'
 const MAIN_ROUTE = 'admin'
 
+var products = []
 router.get(`/${ADD_PRODUCT_ROUTE}`, (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post(`/${ADD_PRODUCT_ROUTE}`, (req, res) => {
-    console.log(req.body)
+    products.push({ title: req.body.title })
     res.redirect('/')
 })
 
-module.exports = router
-
-// const express = require('express');
-
-// const router = express.Router();
-
-// router.get('/add-product', (req, res, next) => {
-//   res.send(
-//     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-//   );
-// });
-
-// router.post('/add-product', (req, res, next) => {
-//   console.log(req.body);
-//   res.redirect('/');
-// });
-
-// module.exports = router;
+module.exports = {
+    routes: router,
+    products,
+}
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,10 @@ const path = require('path')
 const rootDir = require('../utils/path')
 
 const shopRouter = express.Router()
+const adminData = require('./admin')
 
 shopRouter.get('/', (req, res) => {
+    console.log('shop.js', adminData.products)
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
