393ebf2018edb46fe4ec67539ad788a15023b1e9
Asiful Islam Sakib <asif@magnetismtech.com>
2022-10-06 22:07:24 +0600

    fix: 404

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.render('404', { pageTitle: 'Page Not Found', path: '' })
 })
 
 app.listen(4000)
