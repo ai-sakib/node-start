1ac4482fb8d0089c52a59744528fafbddfd2f35f
Asiful Islam Sakib <sakib2439@gmail.com>
2022-10-07 18:20:45 +0600

    feat: edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,29 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true,
+        editMode: false,
+    })
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit
+    if (!editMode) {
+        return res.redirect('/')
+    }
+    console.log(editMode)
+
+    const productId = req.params.productId
+    Product.find(productId, product => {
+        console.log('editProduct', product)
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/add-product',
+            editMode: editMode,
+            product: product,
+        })
     })
 }
 
@@ -15,9 +32,21 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const price = req.body.price
     const description = req.body.description
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null, title, imageUrl, description, price)
     product.save()
     res.redirect('/')
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const id = req.body.productId
+    const title = req.body.title
+    const imageUrl = req.body.imageUrl
+    const price = req.body.price
+    const description = req.body.description
+    const product = new Product(id, title, imageUrl, description, price)
+
+    product.save()
+    res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
